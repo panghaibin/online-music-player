e4aa7d10c34a468c59c6944e0e14a0560789ea65
panghaibin <panghaibin@shu.edu.cn>
2021-10-06 22:43:54 +0800

    doc

--- README.md
+++ README.md
@@ -3,6 +3,39 @@ Online Music Player
 
 基于 [MKOnlineMusicPlayer](https://github.com/mengkunsoft/MKOnlineMusicPlayer) 魔改
 
-新增支持歌词翻译合并，LRC 格式歌词生成下载，PDF 格式歌词下载，歌词区域全屏，墨水屏设备适配。
+项目地址： https://github.com/panghaibin/online-music-player
 
-该代码魔改于 2018 年，~~当时写得很烂就是了~~
+---
+
+## 魔改新增的功能
+
+ - 后端实现外文歌词翻译合并
+   
+ - LRC 格式歌词生成下载，采用 ANSI 编码格式，保证兼容性
+   
+ - 将歌词导出为 PDF 格式并下载，允许以下自定义排版设置：
+
+    - 自定义标题
+   
+     - 字号：6~20
+    
+     - 字体：等线 / 微软雅黑 / ~~华康少女体~~
+   
+     - 分栏设置：不分栏 / 两栏 / 三栏
+   
+     - 显示歌曲二维码：内容为歌曲的主页
+   
+ - 双击歌词区域，可进入全屏显示
+   
+ - 墨水屏设备适配 (/music-ink)
+
+ - 可将歌曲播放链接替换为自建阿里云 OSS 的链接，以解决付费歌曲无法解析问题 (/music/config.php)
+
+## 说明
+原项目使用到了 [Meting](https://github.com/metowolf/Meting) 进行歌曲的解析，如有需要可手动替换更新。另外WYY的歌曲解析需要 Cookies ，若失效需要手动获取替换，位于 /music/config.php
+
+LRC 格式歌词生成下载的功能用到了一个字符编码转换类，目前没有找到原始出处
+
+歌词导出为 PDF 格式并下载功能用到了 [TCPDF](https://tcpdf.org/) 
+
+该代码魔改于 2018 年，~~写得很烂就是了，目前项目代码依赖 Bug 运行~~
--- music/config.php
+++ music/config.php
@@ -3,6 +3,11 @@
 $netease_cookie = 'os=pc; JSESSIONID-WYYY=sPU%2FfYJssxiO%2BMcrC7tswsA2NUBVOOeAZZKVM8QB2XyIWzIZw%2BT8PBIY6%2Bo5MxKKOhtYmX4siYgwBmvs1HsDMSyaSm6%2FSOxudGBxSg3U3ScA6aEpw1MsUeVuah%2Bg7sTI%2BT%2F9VdaX6%5CdZkJT4O%5C1on%2FweQvV1FTEiA%2Fd0K2Sue4Z%2BuCoI%3A1633456998144; _iuqxldmzr_=32; _ntes_nnid=060c9842bf4e7fe4c1539d9a03d3d118,1633455198170; _ntes_nuid=060c9842bf4e7fe4c1539d9a03d3d118; NMTID=00OhhCz0Qh55jlHAEeCpU2Mur_HpgcAAAF8UYRNTA; WNMCID=dfbasv.1633455199453.01.0; WEVNSM=1.0.0; WM_NI=Fk76s%2Bfwxj16mtwaV%2BW8GAgegqoZt02tP9U4I0E1LxeVmbYwzmMyHzkyGjZFt6S7kV9iRcpdVA0TDjUMsHkXG6VxkCXQg6X6G96CS844OuucJRkE%2FmsbvPRc7zuOdGiBT3M%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee8cf77cb2bb8fd7e274909a8ea3d84a878f8bbbf47c818789d1cb4eaa98f9d5e52af0fea7c3b92a86b5f9d7f221a6a9b88bb43eafacb982d52195e889a2e66fa5afa1bbf73cf2aca0dad8408bf08bd2cf74a78aa383dc7dbab3839acb72a9a7898bd94eb8ea9dd8ea5efbb79e84f370969f00b5c7629793bda9ea5485ea9a99ea499c8fa9d7db3e85e7faa4d234f8ec8196f8698a8ea5a6ae67b1bebab1d169f5a6bbb0e74dab9e9cd4d037e2a3; WM_TID=BKL2cBncYGNEFUEQQUZqoWdSwXRiW75P';
 /************ ↑↑↑↑↑ 如果网易云音乐歌曲获取失效，请将你的 COOKIE 放到这儿 ↑↑↑↑↑ ***************/
 
+/**
+ * cookie 获取及使用方法见
+ * https://github.com/metowolf/Meting/wiki/special-for-netease
+*/
+
 //**********OSS 初始化开始**********
 
 $accessKeyId = "填写OSS的 accessKeyId";
